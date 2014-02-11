983470134a1eb1c934ce5564fe9101ebe340c174
Andrew Gallant <jamslam@gmail.com>
2014-02-10 22:15:31 -0500

    Get rid of util and shove that stuff into command.go

--- command.go
+++ command.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/BurntSushi/toml"
 
+	"github.com/BurntSushi/goim/imdb"
 	"github.com/BurntSushi/goim/tpl"
 )
 
@@ -23,6 +24,27 @@ var (
 	flagQuiet      = false
 	flagDb         = ""
 	flagConfig     = ""
+)
+
+var (
+	sf     = fmt.Sprintf
+	ef     = fmt.Errorf
+	pf     = fmt.Printf
+	fatalf = func(f string, v ...interface{}) { pef(f, v...); os.Exit(1) }
+	pef    = func(f string, v ...interface{}) {
+		fmt.Fprintf(os.Stderr, f+"\n", v...)
+	}
+	logf = func(format string, v ...interface{}) {
+		if flagQuiet {
+			return
+		}
+		pef(format, v...)
+	}
+	warnf = func(format string, v ...interface{}) {
+		if flagWarnings {
+			pef(format, v...)
+		}
+	}
 )
 
 func init() {
@@ -196,5 +218,35 @@ func (c *command) assertNArg(n int) {
 func (c *command) assertLeastNArg(n int) {
 	if c.flags.NArg() < n {
 		c.showUsage()
+	}
+}
+
+func createFile(fpath string) *os.File {
+	f, err := os.Create(fpath)
+	if err != nil {
+		fatalf(err.Error())
+	}
+	return f
+}
+
+func openFile(fpath string) *os.File {
+	f, err := os.Open(fpath)
+	if err != nil {
+		fatalf(err.Error())
+	}
+	return f
+}
+
+func openDb(driver, dsn string) *imdb.DB {
+	db, err := imdb.Open(driver, dsn)
+	if err != nil {
+		fatalf("Could not open '%s:%s': %s", driver, dsn, err)
+	}
+	return db
+}
+
+func closeDb(db *imdb.DB) {
+	if err := db.Close(); err != nil {
+		fatalf("Could not close database: %s", err)
 	}
 }
