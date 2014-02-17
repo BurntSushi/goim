9665e78f6afafa658bb7ee2b74833e3423720acb
Andrew Gallant <jamslam@gmail.com>
2014-02-17 18:01:25 -0500

    Documentation and examples for searching.

--- cmd_simple.go
+++ cmd_simple.go
@@ -75,6 +75,90 @@ func init() {
 The search command exposes a flexible interface for quickly searching IMDb
 for entities, where entities includes movies, TV shows, episodes and actors.
 
+A search query has two different components: text to search the names of 
+entities in the database and directives to do additional filtering on 
+attributes of entities (like year released, episode number, cast/credits, 
+etc.). Included in those directives are options to sort the results or specify 
+a limit on the number of results returned.
+
+The search query is composed of whitespace delimited tokens. Each token that 
+starts and ends with a '{' and '}' is a directive. All other tokens are used as 
+test to search the names of entities.
+
+If you're using PostgreSQL with the 'pg_trgm' extension enabled, then text 
+searching is fuzzy. Otherwise, text may contain the wildcard '%%' which matches 
+any sequence of characters or the wildcard '_' which matches any single 
+character. Whenever a wildcard character is used, fuzzy search is disabled (and 
+the search will be case insensitive).
+
+Directives have the form '{NAME[:ARGUMENT]}', where NAME is the name of the 
+directive and ARGUMENT is an argument for the directive. Each directive either 
+requires no argument or requires a single argument.
+
+Examples
+--------
+The following are some example query strings. They can be used in 'goim search'
+as is. Note that examples without wildcards assume that a PostgreSQL database 
+is used with the 'pg_trgm' extension enabled. Some also assume that your 
+database has certain data (for example, the 'actors' list must be loaded to use 
+the 'cast' and 'credits' directives).
+
+Find all entities with names beginning with 'The Matrix' (case insensitive):
+
+  'the matrix%%'
+
+Now restrict those results to only movies:
+
+  'the matrix%%' {movie}
+
+Or restrict them further by only listing movies where Keanu Reeves is a 
+credited cast member:
+
+  'the matrix%%' {movie} {cast:keanu reeves}
+
+Finally, sort the list of movies by IMDb rank and restrict the results to only
+movies with 10,000 votes or more:
+
+  'the matrix%%' {movie} {cast:keanu reeves} {sort:rank desc} {votes:10000-}
+
+We could also search in the other direction, for example, by finding the top
+5 credits in the movie The Matrix:
+
+  {credits:the matrix} {billing:1-5} {sort:billing asc}
+
+If you try this with 'goim search', then you'll get a prompt that 'credits is
+ambiguous' with a list of entities to choose. This can be rather inconvenient
+to see every time. Luckily, directives like 'credits' and 'cast' are actually
+entire sub-searches that support directives themselves. For example, we can 
+specify that the matrix is a movie, which should be enough to make an 
+umabiguous selection:
+
+  {credits:the matrix {movie}} {billing:1-5} {sort:billing asc}
+
+Let's switch gears and look at searching episodes for television shows. For 
+example, we can list the episode names for the first season of The Simpsons:
+
+  {show:simpsons} {seasons:1} {sort:episode asc}
+
+Note here that there is no text to search here. But we could add some if we 
+wanted to, for example, to see all episodes in the entire series with 'bart' in 
+the title:
+
+  {show:simpsons} {sort:season asc} {sort:episode asc} '%%bart%%' {limit:1000}
+
+Note the changes here: we removed the restriction on the first season, added
+a limit of 1000 (since the default limit is 30, but there may be more than 30 
+episodes with 'bart' in the title) and added an additional sorting criteria.
+In this case, we want to sort by season first and then by episode. (The order
+in which they appear in the query matters.)
+
+We can view this data in a lot of different ways, for example, by finding the
+top 10 best ranked Simpsons episodes with more than 500 votes:
+
+  {show:simpsons} {sort:rank desc} {limit:10} {votes:500-}
+
+All search directives
+---------------------
 %s
 `, cmdDoc)
 }
