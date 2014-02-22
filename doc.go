cc7726fd10279c5e2dbfeb522843475ae9b02168
Andrew Gallant <jamslam@gmail.com>
2014-02-21 20:01:46 -0500

    Updated usage.

--- doc.go
+++ doc.go
@@ -5,40 +5,40 @@ media files easily.
 
 Usage:
 
-	goim {command} [flags] [arguments]
+    goim {command} [flags] [arguments]
 
 Use 'goim help {command}' for more details on {command}.
 
 A list of the main commands:
 
-	load            creates/updates database with IMDb data
-	rename          renames files to match search results
-	search          search IMDb for movies, TV shows, episodes and actors
-	size            lists size of tables and total size of database
-	write-config    write a default configuration
+    load      creates/updates database with IMDb data
+    rename    renames files to match search results
+    search    search IMDb for movies, TV shows, episodes and actors
+    size      lists size of tables and total size of database
+    write     write default configuration or templates
 
 A list of other commands:
 
-	aka-titles            show AKA titles for media
-	alternate-versions    show alternate versions for media
-	color-info            show color info for media
-	credits               show actor/media credits
-	full                  show exhaustive information about an entity
-	genres                show genres tags for media
-	goofs                 show goofs for media
-	languages             show language information for media
-	links                 show links (prequels, sequels, versions) of media
-	literature            show literature references for media
-	locations             show geography locations for media
-	mpaa                  show MPAA rating for media
-	plots                 show plot summaries for media
-	quotes                show quotes for media
-	rank                  show user rank/votes for media
-	release-dates         show release dates (by region) for media
-	running-times         show running times (by region) for media
-	short                 show selected information about an entity
-	sound-mix             show sound mix information for media
-	taglines              show taglines for media
-	trivia                show trivia for media
+    aka-titles            show AKA titles for media
+    alternate-versions    show alternate versions for media
+    color-info            show color info for media
+    credits               show actor/media credits
+    full                  show exhaustive information about an entity
+    genres                show genres tags for media
+    goofs                 show goofs for media
+    languages             show language information for media
+    links                 show links (prequels, sequels, versions) of media
+    literature            show literature references for media
+    locations             show geography locations for media
+    mpaa                  show MPAA rating for media
+    plots                 show plot summaries for media
+    quotes                show quotes for media
+    rank                  show user rank/votes for media
+    release-dates         show release dates (by region) for media
+    running-times         show running times (by region) for media
+    short                 show selected information about an entity
+    sound-mix             show sound mix information for media
+    taglines              show taglines for media
+    trivia                show trivia for media
 */
 package main
