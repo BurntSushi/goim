913b3732d77f9e87372227d53a9c72936f1e6d07
Andrew Gallant <jamslam@gmail.com>
2014-02-26 20:47:19 -0500

    Define a few enumerations in the imdb package.
    There are more "enumerations" in the database (even if not explicitly
    declared in the schema). Preferring to add fewer now.

---
+++ imdb/enums.go
@@ -0,0 +1,41 @@
+package imdb
+
+// This file should be auto-generated.
+
+// EnumMPAA lists all available MPAA rating values.
+var EnumMPAA = []string{"G", "PG", "PG-13", "R", "NC-17"}
+
+// EnumGenres lists all available genre attribute values.
+var EnumGenres = []string{
+	"action",
+	"adult",
+	"adventure",
+	"animation",
+	"biography",
+	"comedy",
+	"commercial",
+	"crime",
+	"documentary",
+	"drama",
+	"experimental",
+	"family",
+	"fantasy",
+	"film-noir",
+	"game-show",
+	"history",
+	"horror",
+	"lifestyle",
+	"music",
+	"musical",
+	"mystery",
+	"news",
+	"reality-tv",
+	"romance",
+	"sci-fi",
+	"short",
+	"sport",
+	"talk-show",
+	"thriller",
+	"war",
+	"western",
+}
